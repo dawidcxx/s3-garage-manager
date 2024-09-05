28c8ea9c8253e25deac50966d5eb4c41ccc1fa26
Dawid <foo@example.com>
2024-09-05 14:54:38 +0200

    Tweak toast timeouts a bit for user clarity

--- src/components/Toaster/useToaster.tsx
+++ src/components/Toaster/useToaster.tsx
@@ -1,17 +1,27 @@
 import { useAtom } from 'jotai';
 import { toastAtom } from './toastsAtom';
 import { ToastType } from './Toast';
+import { isNil } from '@/lib/util/isNil';
 
 let toastIdSeq = 0;
-const DEFAULT_TIMEOUT = 10_000;
+const DEFAULT_TIMEOUT = 5_000;
+const DEFAULT_ON_ERROR_TIMEOUT = 10_000;
 
 export function useToaster() {
   const [, setToasts] = useAtom(toastAtom);
 
   return {
-    toast: (message: React.ReactNode, toastType: ToastType = ToastType.Default, timeout = DEFAULT_TIMEOUT) => {
+    toast: (message: React.ReactNode, toastType: ToastType = ToastType.Default, timeout?: number) => {
       const id = toastIdSeq;
       toastIdSeq += 1;
+
+      // Use user provided timeout
+      // If timeout is not provided, use default timeout based on toast type
+      const computedTimeout = isNil(timeout)
+        ? toastType === ToastType.Error
+          ? DEFAULT_ON_ERROR_TIMEOUT
+          : DEFAULT_TIMEOUT
+        : timeout;
 
       setToasts((toasts) => {
         return [...toasts, { id, message, toastType }];
@@ -21,7 +31,7 @@ export function useToaster() {
         setToasts((toasts) => {
           return toasts.filter((toast) => toast.id !== id);
         });
-      }, timeout);
+      }, computedTimeout);
     },
   };
 }
